0ca91a1e2840ff6b5a640075a36156e7e8584cdf
Mextrin <seba3552@hotmail.com>
2026-04-14 13:49:21 +0200

    windows openCV fix
    Added scripts to setup vcpkg and opencv on windows

--- build.bat
+++ build.bat
@@ -2,10 +2,22 @@
 set BUILD_DIR=build
 set EXE_DIR=build/Airchestra_artefacts/Debug/Airchestra.exe
 
-if "%1"=="configure" cmake -B %BUILD_DIR%
+if "%1"=="configure" (
+    git clone https://github.com/microsoft/vcpkg ./vcpkg
+    cd vcpkg
+    /bootstrap-vcpkg.bat
+    cd ..
+    cmake -B %BUILD_DIR% -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake
+)
 if "%1"=="build" cmake --build %BUILD_DIR%
 if "%1"=="run" "./%EXE_DIR%"
-if "%1"=="clean" rd /s /q %BUILD_DIR%
+if "%1"=="clean" (
+    rd /s /q %BUILD_DIR%
+)
+if "%1"=="clean-all" (
+    rd /s /q %BUILD_DIR%
+    rd /s /q vcpkg
+)
 if "%1"=="rebuild" (
     rd /s /q %BUILD_DIR%
     cmake -B %BUILD_DIR%
