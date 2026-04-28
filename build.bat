aaf189dedd3a49965fe7272ddd29ab63839ff753
koukios <petefkoukios@gmail.com>
2026-04-28 13:26:22 +0200

    Fixed windows building

--- build.bat
+++ build.bat
@@ -1,45 +1,78 @@
 @echo off
+setlocal enabledelayedexpansion
+
 set BUILD_DIR=build
-set EXE_DIR=build/Airchestra_artefacts/Debug/Airchestra.exe
+set BUILD_CONFIG=Debug
+set EXE_DIR=%BUILD_DIR%\Airchestra_artefacts\%BUILD_CONFIG%\Airchestra.exe
+set QT_PREFIX=C:/Qt/6.11.0/msvc2022_64
+set HAND_DETECTOR_SCRIPT=%~dp0src\mediapipe\hand_detector.py
 
 if "%1"=="build-all" (
     git submodule update --init --recursive
 
-    if not exist ./vcpkg (
+    if not exist vcpkg (
         git clone https://github.com/microsoft/vcpkg ./vcpkg
         cd vcpkg
-        /bootstrap-vcpkg.bat
+        call bootstrap-vcpkg.bat
         cd ..
     )
 
-    cmake -B %BUILD_DIR%
-    cmake --build %BUILD_DIR%
+    if exist %BUILD_DIR% rd /s /q %BUILD_DIR%
+    cmake -B %BUILD_DIR% -DCMAKE_PREFIX_PATH="%QT_PREFIX%" -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_FLAGS="/Zc:__cplusplus /EHsc"
+    cmake --build %BUILD_DIR% --config %BUILD_CONFIG%
+    exit /b
 )
-if "%1"=="compile" cmake --build %BUILD_DIR%
-if "%1"=="run" (
-    set "ULTEVIS_LAUNCH_HAND_DETECTOR=1"
-    set "ULTEVIS_HAND_DETECTOR_SCRIPT=%~dp0src\mediapipe\hand_detector.py"
-    "./%EXE_DIR%"
-)
-if "%1"=="compile-and-run" (
-    cmake --build %BUILD_DIR%
-    set "ULTEVIS_LAUNCH_HAND_DETECTOR=1"
-    set "ULTEVIS_HAND_DETECTOR_SCRIPT=%~dp0src\mediapipe\hand_detector.py"
-    "./%EXE_DIR%"
-)
-if "%1"=="clean" (
-    call :clean-all 
-)
-if "%1"=="clean-build-run" (
-    rd /s /q %BUILD_DIR%
-    cmake -B %BUILD_DIR%
-    cmake --build %BUILD_DIR%
-    set "ULTEVIS_LAUNCH_HAND_DETECTOR=1"
-    set "ULTEVIS_HAND_DETECTOR_SCRIPT=%~dp0src\mediapipe\hand_detector.py"
-    "./%EXE_DIR%"
-)
-exit
 
-:clean-all
-    rd /s /q %BUILD_DIR%
-exit \b
+if "%1"=="compile" (
+    cmake --build %BUILD_DIR% --config %BUILD_CONFIG%
+    exit /b
+)
+
+if "%1"=="run" (
+    set "PATH=C:\Qt\6.11.0\msvc2022_64\bin;!PATH!"
+    set "ULTEVIS_LAUNCH_HAND_DETECTOR=1"
+    set "ULTEVIS_HAND_DETECTOR_SCRIPT=!HAND_DETECTOR_SCRIPT!"
+
+    echo Running: "!EXE_DIR!"
+    echo Hand detector script: "!ULTEVIS_HAND_DETECTOR_SCRIPT!"
+
+    "!EXE_DIR!"
+    echo Program exited with code !ERRORLEVEL!
+    pause
+    exit /b
+)
+
+if "%1"=="compile-and-run" (
+    cmake --build %BUILD_DIR% --config %BUILD_CONFIG%
+
+    set "PATH=C:\Qt\6.11.0\msvc2022_64\bin;!PATH!"
+    set "ULTEVIS_LAUNCH_HAND_DETECTOR=1"
+    set "ULTEVIS_HAND_DETECTOR_SCRIPT=!HAND_DETECTOR_SCRIPT!"
+
+    "!EXE_DIR!"
+    pause
+    exit /b
+)
+
+if "%1"=="clean" (
+    if exist %BUILD_DIR% rd /s /q %BUILD_DIR%
+    exit /b
+)
+
+if "%1"=="clean-build-run" (
+    if exist %BUILD_DIR% rd /s /q %BUILD_DIR%
+
+    cmake -B %BUILD_DIR% -DCMAKE_PREFIX_PATH="%QT_PREFIX%" -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_FLAGS="/Zc:__cplusplus /EHsc"
+    cmake --build %BUILD_DIR% --config %BUILD_CONFIG%
+
+    set "PATH=C:\Qt\6.11.0\msvc2022_64\bin;!PATH!"
+    set "ULTEVIS_LAUNCH_HAND_DETECTOR=1"
+    set "ULTEVIS_HAND_DETECTOR_SCRIPT=!HAND_DETECTOR_SCRIPT!"
+
+    "!EXE_DIR!"
+    pause
+    exit /b
+)
+
+echo Usage: build.bat {build-all^|compile^|run^|compile-and-run^|clean^|clean-build-run}
+exit /b 1
